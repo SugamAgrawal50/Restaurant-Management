2465922bf0a71bb4f268585aa5fcf36e63faa4c9
Sugam Agrawal <sugam.agrawal@ril.com>
2024-01-18 14:05:21 +0530

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3500;
 
 
 mongoose.connection.once('open', () => {
+    
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
